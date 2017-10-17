abbe6e7439b2d536bfe9c0b7b876f61affc5ab81
Brendan Davis <brendanmd@gmail.com>
2017-10-17 13:48:03 -0700

    changed some of the formatting for readiability

--- test_series.py
+++ test_series.py
@@ -5,13 +5,16 @@ def test_fibonacci01():
     from series import fibonacci
     assert fibonacci(5) == 3
 
+
 def test_lucas01():
     from series import lucas
     assert lucas(5) == 7
 
+
 def test_series01():
     from series import sum_series
     assert sum_series(4) == 2
+
 
 def test_series02():
     from series import sum_series 
