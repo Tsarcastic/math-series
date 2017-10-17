dbf6263407daf5220060ed43e7ad4a349d92b8ad
Brendan Davis <brendanmd@gmail.com>
2017-10-17 13:44:03 -0700

    finished!

--- series.py
+++ series.py
@@ -10,21 +10,43 @@ def fibonacci(n):
     for x in range(0, n - 1):
         new = fibonacci_sequence[-2] + fibonacci_sequence[-1]
         fibonacci_sequence.append(new)
-    #print(fibonacci[n - 1] + " is the " + n + "th # in the Sequence")
+    return fibonacci_sequence[n - 1]
 
 
 """Get the nth number in the Lucas Numbers"""
-
-
 def lucas(n):
     lucas_numbers = [2, 1]
     #n = int(input('Which number in the Lucas Numbers?'))
     for x in range(0, n - 1):
         new = lucas_numbers[-2] + lucas_numbers[-1]
         lucas_numbers.append(new)
-    #print(lucas[n - 1] + " is the " + n + "th # in the Numbers")
+    return lucas_numbers[n - 1]
 
+"""Get the [value] number in a sequence of [first] and [second]."""
 def sum_series(val, first=0, second=1):
-       if (first == 0) and (second == 1):
-        return fibonacci(val):
+    series = [first, second]
+    for x in range(0, val - 1):
+        new = series[-2] + series[-1]
+        series.append(new)
+    return series[val - 1]  
 
+if __name__ == "__main__":
+    print("This module defines functions that implement")
+    print("mathematical series.\n")
+    print("fibonacci(n):")
+    print("\tReturns the nth value in the Fibonacci Sequence")
+    print("fibonacci(5)")
+    print("\t" + str(fibonacci(5)))
+    print("\n")
+    print("lucas(n):")
+    print("\tReturns the nth value in the Lucas Numbers")
+    print("lucas(5)")
+    print("\t" + str(lucas(5)))
+    print("\n")
+    print("sum_series(n, first, second):")
+    print("\tReturns the nth value in a sequence of numbers where the")
+    print("\tnext number in a sequence is the sum of the last two numbers")
+    print("\tThe first and second arguments are the first and second")
+    print("\tnumbers in the sequence.")
+    print("sum_series(3, 2, 3)")
+    print("\t" + str(sum_series(3, 2, 3)))
--- test_series.py
+++ test_series.py
@@ -1,6 +1,18 @@
 import pytest
 
 
-def test_fibonacci():
+def test_fibonacci01():
     from series import fibonacci
     assert fibonacci(5) == 3
+
+def test_lucas01():
+    from series import lucas
+    assert lucas(5) == 7
+
+def test_series01():
+    from series import sum_series
+    assert sum_series(4) == 2
+
+def test_series02():
+    from series import sum_series 
+    assert sum_series(3, 2, 3) == 5
