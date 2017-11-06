582798ce39921c98e2faab27b558dee9c80e3ce1
Philip Werner <philip.r.werner@gmail.com>
2017-11-06 13:33:20 -0800

    h

--- test_series.py
+++ test_series.py
@@ -1,27 +1,30 @@
 import pytest
 
-"""This tests for the fifth number in the Fibonacci Sequence."""
+
 def test_fibonacci01():
+    """This tests for the fifth number in the Fibonacci Sequence."""
     from series import fibonacci
     assert fibonacci(5) == 3
 
 
-"""This tests for the fifth item in the Lucas numbers."""
 def test_lucas01():
+    """This tests for the fifth item in the Lucas numbers."""
     from series import lucas
     assert lucas(5) == 7
 
 
-"""This tests for the fourth item in the sum series function. No value
+
+def test_series01():
+    """This tests for the fourth item in the sum series function. No value
     is entered for the first and second numbers in the sequence so it 
     defaults to the Fibonacci Sequence"""
-def test_series01():
     from series import sum_series
     assert sum_series(4) == 2
 
 
-"""This tests for the third number in the sequence that starts with two
-    and three"""
+
 def test_series02():
+    """This tests for the third number in the sequence that starts with two
+    and three"""
     from series import sum_series 
     assert sum_series(3, 2, 3) == 5
